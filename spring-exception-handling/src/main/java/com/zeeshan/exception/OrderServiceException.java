f8d41b981db5b33de8494f1fa54a06bf34d35d32
ZEESHAN KHAN <zeeshan.maths@gmail.com>
2019-03-11 13:19:00 +0530

    Custom exception class

---
+++ spring-exception-handling/src/main/java/com/zeeshan/exception/OrderServiceException.java
@@ -0,0 +1,14 @@
+package com.zeeshan.exception;
+
+public class OrderServiceException extends Exception {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	public OrderServiceException(String message) {
+		super(message);
+	}
+
+}
